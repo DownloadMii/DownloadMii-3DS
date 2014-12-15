34ba2443c72624e9e703d0bd54891c09ddfaf3d5
Lino <linoma@gmail.com>
2014-12-15 14:47:47 +0100

    Update input.h
    Basic classes for widgets

--- include/input.h
+++ include/input.h
@@ -12,13 +12,50 @@ struct Input_s{
 	u16 touchX = 0, touchY = 0;
 };
 
-struct vButton_s{
-	int ID;
-	int x, y;
-	bool pressed;
+typedef struct __RECT{
+	s32 left,top,right,bottom;
+} RECT,*LPRECT;
+
+struct IMiiWindow{
+	virtual int create(u32,u32,u32,u32,u32) = 0;
+	virtual int destroy() = 0;
+	virtual int draw(u8 *screen) = 0;
+	virtual int onInput(Input_s*) = 0;
+	virtual int set_BkColor(u32 c) = 0;
+	virtual int set_TextColor(u32 c) = 0;
+	virtual int set_Text(char *s) = 0;
 };
 
-extern vector<vButton_s> vButtons;
+class CMiiWindow : public IMiiWindow {
+public:	
+	CMiiWindow();
+	virtual ~CMiiWindow();
+	int create(u32 x,u32 y,u32 w,u32 h,u32 id);
+	virtual int destroy();
+	virtual int draw(u8 *screen);
+	virtual int onInput(Input_s* i);
+	int set_BkColor(u32 c);
+	int set_TextColor(u32 c);
+	int set_Text(char *s);
+protected:
+	u32 ID,color,bkcolor,status,styles,accel;
+	RECT sz;	
+	char *text;
+	int (*cb)(CMiiWindow *);
+};
+
+class vButton_s : public CMiiWindow{
+	int draw(u8 *screen);
+};
+
+class CScrollBar : public CMiiWindow{
+public:
+	int draw(u8 *screen){return 0;};
+protected:
+	u32 pos,min,max;
+};
+
+extern vector<IMiiWindow *> vButtons;
 
 void UpdateInput(Input_s*  input);
 void resetInput(Input_s*  input);
